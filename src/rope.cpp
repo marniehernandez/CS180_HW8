c6b0e5d64b1f465d12a5acfefa43647039838478
marniehernandez <43832443+marniehernandez@users.noreply.github.com>
2021-12-06 22:33:50 -0800

    Add files via upload

---
+++ src/rope.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+#include "CGL/vector2D.h"
+
+#include "mass.h"
+#include "rope.h"
+#include "spring.h"
+
+namespace CGL {
+
+    const float DAMPING_FACTOR = 0.00005;
+
+    Rope::Rope(Vector2D start, Vector2D end, int num_nodes, float node_mass, float k, vector<int> pinned_nodes)
+    {
+        // TODO (Part 1): Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
+    }
+
+    void Rope::simulateEuler(float delta_t, Vector2D gravity)
+    {
+        
+        for (auto &s : springs)
+        {
+            // TODO (Part 2): Use Hooke's law to calculate the force on a node
+        }
+
+        for (auto &m : masses)
+        {
+            if (!m->pinned)
+            {
+                // TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
+            }
+            // TODO (Part 2): Reset all forces on each mass
+        }
+    }
+
+    void Rope::simulateVerlet(float delta_t, Vector2D gravity)
+    {
+        for (auto &s : springs)
+        {
+            // TODO (Part 3): Simulate one timestep of the rope using explicit Verlet
+            // TODO (Part 3): Move each mass by the half displacement
+        }
+
+        for (auto &m : masses)
+        {
+            if (!m->pinned)
+            {
+                Vector2D temp_position = m->position;
+                // TODO (Part 3.1): Set the new position of the rope mass
+                // TODO (Part 4): Add global Verlet damping
+            }
+        }
+    }
+}
